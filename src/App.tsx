99956c524be45e26fb733f8bbb0ec676e3839c2b
sincos <s1nc0s@protonmail.com>
2024-01-02 12:59:12 +0200

    updated with vite

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import "./App.css";
+import { useState } from "react";
+import CryptoJS from "crypto-js";
+
+function App() {
+  const [password, setPassword] = useState("");
+  const [hiddenPassword, setHiddenPassword] = useState("");
+  const [plaintext, setPlaintext] = useState("");
+  const [encrypted, setEncrypted] = useState("");
+
+  const encrypt = (plaintext: string) => {
+    setPlaintext(plaintext);
+    setEncrypted(CryptoJS.AES.encrypt(plaintext, password).toString());
+  };
+
+  const decrypt = (encrypted: string) => {
+    setEncrypted(encrypted);
+    setPlaintext(
+      CryptoJS.AES.decrypt(encrypted, password).toString(CryptoJS.enc.Utf8)
+    );
+  };
+
+  const updatePW = (password: string) => {
+    setPassword(password);
+    let hidden = "";
+    for (let i = 0; i < password.length; i++) {
+      hidden += "*";
+    }
+    setHiddenPassword(hidden);
+  };
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>Hush</h1>
+      </nav>
+      <section>
+        <label>Password</label>
+        <br />
+        <textarea
+          className="password"
+          value={hiddenPassword}
+          onFocus={(e) => {
+            e.target.select();
+          }}
+          onChange={(e) => {
+            updatePW(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <label>Plain Text</label>
+        <br />
+        <textarea
+          className="plaintext"
+          value={plaintext}
+          onFocus={(e) => {
+            e.target.select();
+          }}
+          onChange={(e) => {
+            encrypt(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <label>Encrypted</label>
+        <br />
+        <textarea
+          className="encrypted"
+          value={encrypted}
+          onFocus={(e) => {
+            e.target.select();
+          }}
+          onChange={(e) => {
+            decrypt(e.target.value);
+          }}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default App;
